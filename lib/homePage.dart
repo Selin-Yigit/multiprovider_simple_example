089035ce37f2a396fb556a02d92895b186412008
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 23:47:33 +0300

    Seconds ve Minutes Class'ları oluşturuldu.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+
+  Seconds() : value = now();
+}
+
+@immutable
+class Minutes {
+  final String value;
+
+  Minutes() : value = now();
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
