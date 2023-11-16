6aecddf87d68fcc31f48961bc49c14e3f4f1e2cc
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-16 23:48:26 +0300

    SecondsWidget ve MinutesWidget oluşturuldu.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 String now() => DateTime.now().toIso8601String();
@@ -15,6 +16,38 @@ class Minutes {
   final String value;
 
   Minutes() : value = now();
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        height: 100,
+        color: Colors.yellow.shade200,
+        child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        height: 100,
+        color: Colors.lime.shade400,
+        child: Text(minutes.value),
+      ),
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
